09bf2f3a9c74b2aef6a472555883319257cd623f
AwaizSh4h <awaizshah954@gmail.com>
2026-02-19 01:30:32 +0500

    phr sy

---
+++ Stack.cpp
@@ -0,0 +1,15 @@
+#include <stack>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+stack<string> cars;
+stack<string> cars1= {"Volvo", "BMW", "Ford", "Mazda"};
+
+// Add elements to the stack
+cars.push("Volvo");
+cars.push("BMW");
+cars.push("Ford");
+cars.push("Mazda");
+}
---
+++ search.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Linear Search 
+
+int search(vector<int>& arr, int x) {
+    
+    // Iterate over the array in order to find the index of the key x. If the key is not found, return -1.
+    for (int i = 0; i < arr.size(); i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {2, 3, 4, 10, 40};
+    int x = 10;
+    int res = search(arr, x);
+    if (res == -1)
+       cout << "Element is not present in the array";
+    else
+       cout << "Element is present at index " << res;
+    return 0;
+}
